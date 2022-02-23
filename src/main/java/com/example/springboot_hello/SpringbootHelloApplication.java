5d52b9ade6d3c298a3dcfb72f7ad02f787575626
yjj <mf1732159@smail.nju.edu.cn>
2022-02-23 13:56:44 +0800

    clean code

--- src/main/java/com/example/springboot_hello/SpringbootHelloApplication.java
+++ src/main/java/com/example/springboot_hello/SpringbootHelloApplication.java
@@ -1,14 +1,34 @@
 package com.example.springboot_hello;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 
 @SpringBootApplication
-public class SpringbootHelloApplication {
+@Slf4j
+public class SpringbootHelloApplication implements CommandLineRunner {
+
+    @Autowired
+    DataSource dataSource;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootHelloApplication.class, args);
+    }
+
+    @Override
+    public void run (String... args) throws Exception{
+        log.info("dataSource.toString()");
+        log.info(dataSource.toString());
+        Connection conn = dataSource.getConnection();
+        log.info("conn.toString()");
+        log.info(conn.toString());
+        conn.close();
     }
 
 }
--- src/main/java/com/example/springboot_hello/entity/User.java
+++
@@ -1,9 +0,0 @@
-//package com.example.springboot_hello.entity;
-//
-//import lombok.Data;
-//
-//@Data
-//public class User {
-//    private Integer id;
-//    private String name;
-//}
